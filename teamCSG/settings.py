dc57d80d04ef70d60e2c1c90ac236de4780f1eb2
Deyoungj <emmanuelonyibor39@gmail.com>
2022-09-14 17:15:54 +0100

    cleared error

--- teamCSG/settings.py
+++ teamCSG/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'user_control.apps.UserControlConfig',
 ]
 
 ROOT_URLCONF = 'teamCSG.urls'
