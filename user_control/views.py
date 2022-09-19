445bbc9f2b6ab16b7dc0468be819adce786eb68f
Deyoungj <emmanuelonyibor39@gmail.com>
2022-09-19 13:52:47 +0100

    meview

--- user_control/views.py
+++ user_control/views.py
@@ -1,6 +1,7 @@
 from ast import Is
 from rest_framework.viewsets import ModelViewSet
-from .serializers import CreateUserSerializer, CustomUser, LoginSerializer
+from .serializers import (CreateUserSerializer,
+ CustomUser, LoginSerializer, UpdatePasswordSerializer)
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from datetime import datetime
@@ -12,7 +13,7 @@ class UserViewSet(ModelViewSet):
     http_method_names = ['post']
     serializer_class= CreateUserSerializer
     queryset = CustomUser.objects.all()
-    permission_classes = (IsAuthenticatedCustom)
+    permission_classes = (IsAuthenticatedCustom,)
 
     def create(self, request):
         valid_request =  self.serializer_class(data=request.data)
@@ -62,4 +63,23 @@ class LoginViewset(ModelViewSet):
         return Response({'access': access}, status=status.HTTP_200_OK)
             
 
+class UpdatePasswordViewset(ModelViewSet):
+    http_method_names = ['post']
+    serializer_class = UpdatePasswordSerializer
+    queryset = CustomUser.objects.all()
 
+    def create(self, request):
+        valid_request = self.serializer_class(data=request.data)
+        valid_request.is_valid(raise_exception=True)
+
+        user = CustomUser.objects.filter(id=valid_request.validated_data["user_id"]).first()
+
+        if not user:
+            return Response({'error': 'User not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = user[0]
+
+        user.set_password(valid_request.validated_data["password"])
+        user.save()
+
+        return Response({'success': 'password changed successfully'})
