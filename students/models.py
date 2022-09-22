01df4f96811d806db3680d2e07d48fd03c78cf54
Deyoungj <emmanuelonyibor39@gmail.com>
2022-09-22 17:14:52 +0100

    added student app in settings

--- students/models.py
+++ students/models.py
@@ -6,8 +6,7 @@ from random import choice
 import string
 
 def generate_student_id():
-    id_code = ''.join(choice(string.ascii_letters + string.digits) for _ in range(15))
-    return id_code
+    return ''.join(choice(string.ascii_letters + string.digits) for _ in range(15))
 
 GENDER_CHOICES = [('male', 'male'), ('female', 'female'),]
 
@@ -17,37 +16,38 @@ class Package(models.Model):
     months = models.PositiveIntegerField()
 
     def __str__(self):
-        return f'{self.name} {self.price}'
+        return f'{self.name} -> {self.price}'
 
 class Student(models.Model):
     fullname = models.CharField(max_length=255)
-    gender = models.CharField(choices=GENDER_CHOICES)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=7)
     email = models.EmailField(max_length=150, unique=True)
     phone_number = models.CharField(max_length=15, null=True)
     address = models.CharField(max_length=255,null=True)
-    student_id = models.CharField(default=generate_student_id())
-    image = models.FileField(upload_to='images')
+    student_id = models.CharField(editable=False, unique=True, max_length=20,default=generate_student_id)
+    image = models.ImageField(upload_to='images', default='default.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return f'{self.fullname} {self.email}'
+        return f'{self.fullname} -> {self.email}'
 
 
 class PackageEnroled(models.Model):
-    package = models.ForeignKey(Package, related_name='packages', on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, related_name='students', on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, related_name='package', on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, related_name='student', on_delete=models.CASCADE)
     date_enrolled = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField(null=True)
+    due_date = models.DateTimeField(null=True, blank=True)
 
 
     def save(self,*args, **kwargs):
         months= self.package.months
-        due_date = datetime.now() + relativedelta(months=months)
-        return super().save(*args, **kwargs)
+        self.due_date = datetime.now() + relativedelta(months=int(months))
+        
+        return super(PackageEnroled, self).save(*args, **kwargs)
 
 
     def __str__(self):
-        return f'{self.student.fullname} {self.packages.name} on {self.date_enrolled}'
+        return f'{self.student.fullname} {self.package.name} on {self.date_enrolled}'
 
 
--- teamCSG/settings.py
+++ teamCSG/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
