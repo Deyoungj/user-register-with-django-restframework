3c1c3df8a7275d8f0ecda158ded24861a36a09e0
Deyoungj <emmanuelonyibor39@gmail.com>
2022-09-25 10:24:32 +0100

    added student url

--- students/urls.py
+++ students/urls.py
@@ -8,3 +8,5 @@ router = routers.DefaultRouter()
 router.register('create-student', StudentView, 'create_student')
 router.register('package', PackageView, 'package')
 router.register('enroll-student', EnrollStudentView, 'enroll-student')
+
+urlpatterns = router.urls
