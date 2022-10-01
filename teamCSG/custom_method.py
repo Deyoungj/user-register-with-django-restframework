3c5d8d640ed6a545e028aa2502d8b5bd4cb912fb
Deyoungj <emmanuelonyibor39@gmail.com>
2022-10-01 22:58:50 +0100

    view changes

--- teamCSG/custom_method.py
+++ teamCSG/custom_method.py
@@ -4,22 +4,23 @@ from user_control.utils import decodeJWT
 
 class IsAuthenticatedCustom(BasePermission):
     
-    def has_permission(self, request, view):
-        try:
-          auth_token = request.Meta.get("HTTP_AUTHORIZATION",None)
-          print(auth_token)
-        except Exception:
-          return False
-        
-        if not auth_token:
-            return False
+  def has_permission(self, request, view):
+    print('isauthenticated')
+    try:
+      auth_token = request.Meta.get("HTTP_AUTHORIZATION",None)
+      print(auth_token)
+    except Exception:
+      return False
+    
+    if not auth_token:
+        return False
 
-        user = decodeJWT(auth_token)
+    user = decodeJWT(auth_token)
 
-        if not user:
-            return False
+    if not user:
+        return False
 
-        request.user = user
-        return True
+    request.user = user
+    return True
 
 
